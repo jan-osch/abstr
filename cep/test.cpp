0f6901a8a0a73d4778c5300c009309bb6ea7d7bc
janek_g <jmgrzesik@gmail.com>
2016-11-10 19:25:47 +0100

    Add iterators

--- cep/test.cpp
+++ cep/test.cpp
@@ -14,9 +14,9 @@ class Animal {
 private:
     int age;
 public:
-
     string name;
-    int getAge() const{
+
+    int getAge() const {
         return age;
     }
 
@@ -25,43 +25,66 @@ public:
         age = _age;
     }
 
-    Animal(string _name): name(_name) {}
+    /**
+     * Konstruktor z listą inicjacyjną
+     * @param _name
+     * @param _age
+     * @return
+     */
+    Animal(string _name, int _age) : name(_name), age(_age) {};
 
-    Animal(const Animal& obj) :age(obj.getAge()), name(obj.name) {}
-    virtual ~Animal()= 0;
+    /**
+     * Konstruktor kopiujący
+     * @param obj
+     * @return
+     */
+    Animal(const Animal &obj) {
+        age = obj.getAge();
+        name = obj.name;
+    }
+
+    Animal() {}
 };
 
 
 class Cat : public Animal, public ISpeakable {
+private:
+    string color;
 public:
     string speak() {
         return "Meow meow";
     }
 };
 
+
 class Dog : public Animal, public ISpeakable {
 public:
-    Dog(const Animal& obj): Animal(obj){}
     string speak() {
         return "bark bark";
     }
 };
 
+
 class Fish : public Animal {
-public:
 };
 
 
 int main() {
-    Dog *dog = new Dog("Reksio");
-    cout << dog->speak() << "\n";
+    Dog *dog = new Dog;
+    dog->setAge(1);
+    dog->name = "Reksio";
+    cout << dog->speak() << endl;
 
-    Cat *cat = new Cat("Mruczek");
+    Cat *cat = new Cat;
+    cat->setAge(8);
+    cat->name = "Behemot";
     cout << cat->speak() << "\n";
 
-    Fish *fish = new Fish("Nemo");
 
-    cout << fish->name<< "\n";
+    Fish *fish = new Fish;
+    fish->name = "Nemo";
+
+    cout << fish->name << "\n";
 
     return 0;
 }
